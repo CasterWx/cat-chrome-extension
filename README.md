024a0510e7e762debc291785cffe24b7ab526d2d
AntzUhl <a@aloli.icu>
2020-08-21 18:17:46 +0800

    feature(conf/code): 下拉显示表头

---
+++ README.md
@@ -0,0 +1,6 @@
+## Cat-chrome-extension
+
+#### 功能1: 下拉显示表头
+
+![img1](images/img1.jpg)
+
---
+++ js/cat-th.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  $("th.right").css("position","sticky");
+  $("th.right").css("top","20px");
+
+  console.log("cat~")
+
+})();
